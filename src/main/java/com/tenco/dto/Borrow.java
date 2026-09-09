8715a46abdbbada265c234f0ca31abc343fd3082
수현 <i.suhyun1024@gmail.com>
2026-09-09 11:29:52 +0900

    DTO 설계

--- src/main/java/com/tenco/dto/Borrow.java
+++ src/main/java/com/tenco/dto/Borrow.java
@@ -10,8 +10,8 @@ import java.time.LocalDate;
 @ToString
 public class Borrow {
     private int id;
-    private int book_id;
-    private int student_id;
-    private LocalDate borrow_date;
-    private LocalDate return_date;
+    private int bookId;
+    private int studentId;
+    private LocalDate borrowDate;
+    private LocalDate returnDate;
 }
--- src/main/java/com/tenco/dto/Student.java
+++ src/main/java/com/tenco/dto/Student.java
@@ -10,5 +10,5 @@ import lombok.*;
 public class Student {
     private int id;
     private String name;
-    private String student_id;
+    private String studentId;
 }
