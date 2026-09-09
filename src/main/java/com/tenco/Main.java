9b056e0655865ba7acfbc353f938172a9a313366
수현 <i.suhyun1024@gmail.com>
2026-09-09 16:11:00 +0900

    Book DAO 설계

--- src/main/java/com/tenco/Main.java
+++ src/main/java/com/tenco/Main.java
@@ -1,15 +1,44 @@
 package com.tenco;
 
+import com.tenco.dao.BookDAO;
 import com.tenco.dao.StudentDAO;
+import com.tenco.dto.Book;
+import com.tenco.dto.Student;
+
+import java.util.List;
 
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        // 학생 전체 조회 테스트
         StudentDAO studentDAO = new StudentDAO();
-        studentDAO.getAllStudent();
+        // 학생 전체 조회 테스트
+        List<Student> studentList = studentDAO.getAllStudent();
         // 학생 학번 조회 테스트
+        Student student = studentDAO.getStudentById("20230003");
+        System.out.println(student);
 
+        // =======================================
+
+        BookDAO bookDAO = new BookDAO();
+        // 도서 전체 조회 테스트
+        List<Book> bookList = bookDAO.getAllBook();
+        // 도서 제목 검색 테스트
+        List<Book> bookList2 = bookDAO.getBookByTitle("입문");
+        for (int i = 0; i < bookList2.size(); i++){
+            System.out.println(bookList2.get(i));
+        }
+        // 도서 등록 테스트
+        Book book = new Book("테스트책", "저자", "한빛미디어", 2026, "9788968481239", true);
+        bookDAO.addBook(book);
+
+        Book book2 = Book.builder()
+                .author("저자2")
+                .title("테스트2")
+                .publisher("출판사2")
+                .publicationYear(2026)
+                .isbn("12341234123")
+                .build();
+        bookDAO.addBook(book2);
     }
 }
--- src/main/java/com/tenco/dao/BookDAO.java
+++ src/main/java/com/tenco/dao/BookDAO.java
@@ -1,4 +1,100 @@
 package com.tenco.dao;
 
+import com.tenco.dto.Book;
+import com.tenco.dto.Student;
+import com.tenco.util.DatabaseUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookDAO {
+
+    // 도서 전체 검색 기능
+    public List<Book> getAllBook() {
+        List<Book> bookList = new ArrayList<>();
+        String sql = """
+                SELECT * FROM books
+                ORDER BY id
+                """;
+        try (Connection connect = DatabaseUtil.getConnection()) {
+            try (PreparedStatement pstmt = connect.prepareStatement(sql)) {
+                ResultSet rs = pstmt.executeQuery();
+
+                while (rs.next()) {
+                    bookList.add(createBook(rs));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return  bookList;
+    }
+
+    // 제목으로 도서 검색 기능
+    public List<Book> getBookByTitle(String title) {
+        List<Book> bookList = new ArrayList<>();
+        String sql = """
+                SELECT * FROM books
+                WHERE title LIKE ?
+                ORDER BY id
+                """;
+
+        try (Connection connect = DatabaseUtil.getConnection()) {
+            try (PreparedStatement pstmt = connect.prepareStatement(sql)) {
+                pstmt.setString(1, '%' + title + '%');
+
+                ResultSet rs = pstmt.executeQuery();
+                while (rs.next()) {
+                    bookList.add(createBook(rs));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return bookList;
+    }
+
+    // 도서 등록 기능
+    public int addBook(Book book) {
+        int rows = 0;
+        String sql = """
+                INSERT INTO books(title, author, publisher, publication_year, isbn, available)
+                VALUES (?, ?, ?, ?, ?, ?)
+                """;
+
+        try (Connection connect = DatabaseUtil.getConnection()) {
+            try (PreparedStatement pstmt = connect.prepareStatement(sql)) {
+                pstmt.setString(1, book.getTitle());
+                pstmt.setString(2, book.getAuthor());
+                pstmt.setString(3, book.getPublisher());
+                pstmt.setInt(4, book.getPublicationYear());
+                pstmt.setString(5, book.getIsbn());
+                pstmt.setBoolean(6, book.isAvailable());
+                rows = pstmt.executeUpdate();
+                System.out.println(rows + "행이 추가되었습니다");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return rows;
+    }
+
+
+    // 메서드 추출
+    private static Book createBook(ResultSet rs) throws SQLException {
+        Book book = new Book();
+        book.setId(rs.getInt("id"));
+        book.setTitle(rs.getString("title"));
+        book.setAuthor(rs.getString("author"));
+        book.setPublisher(rs.getString("publisher"));
+        book.setPublicationYear(rs.getInt("publication_year"));
+        book.setIsbn(rs.getString("isbn"));
+        book.setAvailable(rs.getBoolean("available"));
+        return book;
+    }
 }
