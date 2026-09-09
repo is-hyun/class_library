62088cece7597839117b20a3f5f4627f927b6d13
수현 <i.suhyun1024@gmail.com>
2026-09-09 12:30:43 +0900

    Student DAO 설계

--- src/main/java/com/tenco/dao/StudentDAO.java
+++ src/main/java/com/tenco/dao/StudentDAO.java
@@ -5,14 +5,20 @@ import com.tenco.util.DatabaseUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentDAO {
 
     // 학생 등록 기능
-    public void addStudent(Student student) {
+    // TODO - 추후 사용하는 측 확인해서 리턴 타입 변경
+    public int addStudent(Student student) {
+        int rows = 0;
+
         String sql = """
-                INSERT INTO student(name, student_id)
+                INSERT INTO students(name, student_id)
                 VALUES (?, ?)
                 """;
         try (Connection connect = DatabaseUtil.getConnection()) {
@@ -24,10 +30,71 @@ public class StudentDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return rows;
     }
 
     // 학생 전체 조회 기능
+    public List<Student> getAllStudent() {
+        List<Student> studentList = new ArrayList<>();
+        String sql = """
+                SELECT * FROM students
+                """;
 
+        try (Connection connect = DatabaseUtil.getConnection()) {
+            try (PreparedStatement pstmt = connect.prepareStatement(sql)) {
+                ResultSet rs = pstmt.executeQuery();
+
+                while (rs.next()) {
+                    Student student = new Student();
+                    student.setId(rs.getInt("id"));
+                    student.setName(rs.getString("name"));
+                    student.setStudentId(rs.getString("student_id"));
+
+                    studentList.add(student);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        /*
+        for (int i = 0; i < studentList.size(); i++) {
+            System.out.println(studentList.get(i));
+        }
+        */
+        return studentList;
+    }
 
     // 학생 학번 조회 기능
+    public List<Student> getIdStudent(String studentId) {
+        List<Student> studentList = new ArrayList<>();
+        String sql = """
+                SELECT * FROM students
+                WHERE student_id = ?
+                """;
+
+        try (Connection connect = DatabaseUtil.getConnection()) {
+            try (PreparedStatement pstmt = connect.prepareStatement(sql)) {
+                pstmt.setString(1, studentId);
+                ResultSet rs = pstmt.executeQuery();
+
+                while (rs.next()) {
+                    Student student = new Student();
+                    student.setId(rs.getInt("id"));
+                    student.setName(rs.getString("name"));
+                    student.setStudentId(rs.getString("student_id"));
+
+                    studentList.add(student);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        /*
+        for (int i = 0; i < studentList.size(); i++) {
+            System.out.println(studentList.get(i));
+        }
+        */
+        return studentList;
+    }
+
 }
